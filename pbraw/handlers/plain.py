e6f0c6c5ae0220bd3701c45d27b265f6041562a9
Mykyta Holubakha <hilobakho@gmail.com>
2017-07-13 04:17:56 +0300

    Improved rfc 6266, 5987 support in plain handler

--- pbraw/handlers/plain.py
+++ pbraw/handlers/plain.py
@@ -1,14 +1,28 @@
 from os import path
 import re
+try:
+    from urllib.parse import unquote
+    py = 3
+except ImportError:
+    from urllib import unquote
+    py = 2
 
 from pbraw import dispatcher
 from pbraw.util import urlparse
 
-CD_FILENAME_RE = re.compile('.*?;\s*filename\s*\*?\s*=\s*(?:\"(.*)\"\s*|(.*))', re.G, re.I, re.X)
+CD_FILENAME_RE = re.compile('.*?;\s*filename\s*(\*?)\s*=\s*(?:\"(.*)\"\s*|(.*))', re.G, re.I, re.X)
+
+def handle(string):
+    enc, _, string = string.partition('\'')
+    lang, _, content = string.partition('\'')
+    enc, content = enc.strip(), content.strip()
+    if py == 3:
+        return unquote(content, enc)
+    else:
+        return unquote(content).decode(enc)
 
 @dispatcher.handler(priority=0)
 def grab_plain(url, req):
-    # TODO: properly implement RFC 6266 (Content-Disposition), 5987 (encodings)
     if req.headers['content-type'].startswith('text/html'):
         return False
     try:
@@ -16,8 +30,9 @@ def grab_plain(url, req):
     except ValueError: # invalid encoding
         return False
     if 'content-disposition' in req.headers:
-        name = CD_FILENAME_RE.search(req.headers['content-disposition']) 
-        if name: name = name.group(0)
+        for m in CD_FILENAME_RE.search(req.headers['content-disposition']):
+            star, filename = m.group(1), m.group(2) or m.group(3)
+            name = handle(filename) if star else filename
     if not name:
         name = path.split(urlparse(url).path)
         if name: name = name[-1]
